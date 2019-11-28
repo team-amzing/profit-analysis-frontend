791cc06ed7421b260ce6ebd2b85e7055e5e80a75
Hattie Stewart <bi19158@IT042527.wks.bris.ac.uk>
2019-11-28 11:01:07 +0000

    Change API key to a constant.

--- get_data/get_data.py
+++ get_data/get_data.py
@@ -1,15 +1,22 @@
 """
 WIT oil price data is acquired from the Quandl API for use by the data_analysis module.
 """
+
 import quandl
+
+# API key for Michael's account
+API_KEY = "NszGhwY_Qh8Ubj1BWhVt"
+
+# TODO: Create team API key and move API_KEY to constants file
 
 
 def call_api(n_days):
     """
-	Calls the api using Michael's key, and returns the last n_days worth of WTI oil price data
-	"""
-    # Set quandl API key
-    quandl.ApiConfig.api_key = "NszGhwY_Qh8Ubj1BWhVt"  # change this to a global constant, maybe even use the team key
+    Calls the API using the given API key, and returns the last n_days worth of WTI oil price data
+    """
+
+    # Declare Quandl API key
+    quandl.ApiConfig.api_key = API_KEY
 
     # Get data from the quandl api, then only return the number of days asked for
     data = quandl.get("EIA/PET_RWTC_D", returns="numpy")
