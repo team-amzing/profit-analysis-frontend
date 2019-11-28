424f453f36deac6af6bb2d36e9aa8d8698684de9
Michael Norman <michael@norvett.com>
2019-11-28 13:39:36 +0000

    Attempted to add tkinter to pipfile, no version found. Added data table at base of UI.

--- frontend/benWinterStart.py
+++ frontend/benWinterStart.py
@@ -5,6 +5,15 @@ from pandas import DataFrame
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+def createLabel(root):
+  # Function to create tkinter label.
+  var   = tk.StringVar()
+  label = tk.Label( root, textvariable=var, anchor="e")
+
+  var.set("Default")
+
+  return label, var
+
 Data1 = {'Day': ['Tuesday','Wednesday','Thursday','Friday','Today','Tomorrow','Wednesday','Thursday','Friday'],
         'Oil_Price': [55.5,60.1,62.9,61.7,63.1,62.4,62.1,59.7,63.9]
        }
@@ -25,6 +34,11 @@ dateFrame = tk.Frame(root)
 dateFrame.pack(fill=tk.X)
 timeFrame = tk.Frame(root)
 timeFrame.pack(fill=tk.X)
+
+#Frame to hold quantative price information:
+labelFrame = tk.Frame(root)
+labelFrame.pack(side = tk.BOTTOM, fill='both', expand=True)
+
 decision = 1
 photo = tk.PhotoImage(file='Logo.png')
 photo_label = tk.Label(root, image =photo).pack(side=tk.TOP)
@@ -58,9 +72,45 @@ x = datetime.datetime.now()
 dateLabel = tk.Label(dateFrame, text=x.strftime("%x"), fg="black")
 timeLabel = tk.Label(timeFrame, text=x.strftime("%X"), fg="black")
 
-
+decision = 1
+if decision == 1:
+    decisionLabel = tk.Label(dateFrame, text="SELL SELL SELL", fg="green")
+else:
+    decisionLabel = tk.Label(dateFrame, text="We advise you not to sell today", fg="red")
 
 dateLabel.pack(side=tk.LEFT, anchor=tk.N, padx=5, pady=5)
 timeLabel.pack(side=tk.LEFT, anchor=tk.N, padx=5, pady=5)
+
+
+
+#Configure grid arangment
+num_rows = 3
+num_columns = 2
+
+total_num_labels = num_rows*(num_columns * 2)
+
+labels     = [[]]*total_num_labels
+label_text = [[]]*total_num_labels
+
+label_idx = 0
+for column in range(num_columns):
+  for row in range(num_rows):
+
+    #Set day columns:
+    labels[2*label_idx], label_text[2*label_idx] = createLabel(labelFrame)
+
+    label_text[2*label_idx].set("{}:".format(Data1['Day'][label_idx]))
+    labels[2*label_idx]    .grid(row=row,column=2*column, sticky='e')
+
+    #Set price columns
+    labels[2*label_idx + 1], label_text[2*label_idx + 1] = createLabel(labelFrame)
+
+    label_text[2*label_idx + 1].set("{}".format(Data1['Oil_Price'][label_idx]))
+    labels    [2*label_idx + 1].grid(row=row,column= 2*column + 1, sticky='w')
+
+    label_idx += 1
+
+  tk.Grid.columnconfigure(labelFrame, 2*column, weight=1)
+  tk.Grid.columnconfigure(labelFrame, 2*column + 1, weight=1)
 
 root.mainloop()
