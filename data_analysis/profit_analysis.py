05bd700268e43084e318889b7e6b677cc1cb9546
TonichaC <55878192+TonichaC@users.noreply.github.com>
2020-01-08 16:05:05 +0000

    Just some small changes

--- data_analysis/profit_analysis.py
+++ data_analysis/profit_analysis.py
@@ -2,7 +2,7 @@
 Using the predictions created in price_prediction.py the profit margin is analysed
 for each prediction.
 """
-import pandas as pd
+
 from data_analysis.price_prediction import model_arima
 
 # Information on the Tanker
@@ -17,15 +17,14 @@ def sell_function(data_points,n_days):
 	# And compares it with the highest profit so far.
 	count = 1
 	best_profit = 0
-	no_rows = prediction_df.shape[0]
 	price = prediction_df.iloc[0]['Prediction']
 	pot_profit = no_units*price
-	while count < no_rows:
+	while count < n_days:
 		price = prediction_df.iloc[count]['Prediction']
 		profit = no_units*price - running_cost*count
 		if pot_profit < profit:
 			best_profit = count
 			pot_profit = profit
 		count += 1
-	print('Sell in %i day' % best_profit)
+	print('Sell in %i days' % best_profit)
 
