1abd3a7500f39ffde9cf4cffd03d629307057492
Tonicha Crook <1915995@swansea.ac.uk>
2020-02-03 14:38:08 +0000

    Updated function to output sell result and a profit array

--- data_analysis/price_prediction.py
+++ data_analysis/price_prediction.py
@@ -17,12 +17,14 @@ def model_arima(n_days, n_predictions):
 
     data = call_api(n_days)
     df = pd.DataFrame(data)
-
+    
     df = df.drop("Date", axis=1)
 
-    model = ARIMA(df, order=(1, 1, 1))
+    model = ARIMA(df[['Value']], order=(1, 1, 1))
     fitted = model.fit(disp=0)
 
     # Forecast, standard error, confidence region for a confidence of 95%
     forecast, error, conf = fitted.forecast(n_predictions, alpha=0.05)
     return pd.DataFrame(data={"Prediction": forecast, "Error": error})
+
+
